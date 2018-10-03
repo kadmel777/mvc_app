ebc89abadedeee537f647d216d2ccd8de09b826b
Juan Parra <juandkpa@gmail.com>
2018-10-03 00:39:56 -0500

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :publications
   resources :authors
-  get 'main/index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
 end
